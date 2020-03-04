3fb509071c43d5cdd3b15cf154fdde6cfa2c1675
Sumair Ahmed <snipered@hotmail.com>
2020-03-04 15:42:37 +0000

    First working example

---
+++ DFC.App.FindACourse/wwwroot/JavaScript/FindACourse.ts
@@ -0,0 +1,26 @@
+﻿let filterService;
+
+$(document).ready(() => {
+    filterService = new FilterService('', 'get', '');
+    var distanceElement = (<HTMLSelectElement>document.getElementById("DistanceValue"));
+    $(distanceElement).on("change", () => CallAPI());
+});
+
+function CallAPI() {
+    filterService.FilterData(GetCurrentStateOfFilters());
+}
+
+function GetCurrentStateOfFilters() {
+    //let returnStateType: string;
+
+    ////let location = document.getElementById("");
+    //// let distance = (<HTMLSelectElement>document.getElementById("DistanceValue"));
+    ////let courseType = document.getElementById("");
+    //let distance = $("#DistanceValue option:selected").val();
+    //let searchTerm = $("#CurrentSearchTerm").text();
+    //let currentPage = $("#CurrentPage").text();
+
+    //returnStateType = "searchTerm=" + searchTerm + "&distance=" + distance + "&page=" + currentPage;
+    //return returnStateType;
+    return $('form').serialize();
+}
---
+++ DFC.App.FindACourse/wwwroot/JavaScript/services/FilterService.ts
@@ -0,0 +1,25 @@
+﻿class FilterService {
+    private endPoint: string;
+    private type: string;
+    private outputId: string;
+
+    constructor(endPoint: string, type: string, outputId: string) {
+        this.endPoint = endPoint;
+        this.type = type;
+        this.outputId = outputId;
+    }
+
+    FilterData(paramFormData: string) {
+        let formData = paramFormData;
+
+        $.ajax({
+            type: this.type,
+            url: this.endPoint,
+            data: formData,
+            success: function (data) {
+                $(this.outputId).empty();
+                $(this.outputId).html(data);
+            }
+        });
+    }
+}
