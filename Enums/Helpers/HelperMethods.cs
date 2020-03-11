94dcb73506933e3a3f3230f0f377785d39913efd
Sumair Ahmed <snipered@hotmail.com>
2020-03-11 09:51:44 +0000

    WIP

---
+++ Enums/Helpers/HelperMethods.cs
@@ -0,0 +1,31 @@
+﻿
+namespace DFC.App.FindACourse.Helpers
+{
+    public static class HelperMethods
+    {
+        public static string Capitalize(string sentance) 
+        {
+            char[] array = sentance.ToCharArray();
+
+            if (array.Length >= 1)
+            {
+                if (char.IsLower(array[0]))
+                {
+                    array[0] = char.ToUpper(array[0]);
+                }
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] == ' ')
+                {
+                    if (char.IsLower(array[i]))
+                    {
+                        array[i] = char.ToUpper(array[i]);
+                    }
+                }
+            }
+            return new string(array);
+        }
+    }
+}
